ecb315cb2dd32aa431736f9f9fb9480612311420
dmitriyvelichkodev <dmitriy.velichko.dev@gmail.com>
2020-09-19 20:12:53 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Initial service
+
+Server is a coordinator between:
+- `oms` - order management system
+- `crm` - customer relation management system
+- `inventory` - warehouse management system
+
+## development
+- `nvm use`
+- `npm i`
+- `npm start`
+
+## notes
+All test creds are stored in `config/default.js`
+
---
+++ config/default.js
@@ -0,0 +1,18 @@
+/*
+ For the test task all services looks to the same mock server. Do not pay attention on that.
+*/
+const MOCK_SERVER = 'b24b202e-cc1f-4520-acdf-9af7d1f7196f.mock.pstmn.io';
+
+module.exports = {
+  port: 3003,
+  host: 'localhost',
+  crm: {
+    host: MOCK_SERVER,
+  },
+  oms: {
+    host: MOCK_SERVER,
+  },
+  inventory: {
+    host: MOCK_SERVER,
+  }
+};
